5935a0ddf8bb222292616cd7f6b71c817121211d
Saarule <saaryazdi@gmail.com>
2023-08-11 19:09:44 +0300

    Update FrensOnChain.sol

--- contracts/contracts/FrensOnChain.sol
+++ contracts/contracts/FrensOnChain.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Base64.sol";
 import "./FrenAlive.sol";
+import "./FrenImage.sol";
 
 
 contract FrensOnChain is ERC721Enumerable, Ownable {
